98d91bcfb3dae91f0ea83d6c6fec92cdc0cbff38
Alexander Globa <grobych@gmail.com>
2022-04-27 12:18:49 +0300

    nullable id and Subject

--- app/src/main/java/com/globa/homeworknotifier/model/Task.kt
+++ app/src/main/java/com/globa/homeworknotifier/model/Task.kt
@@ -2,14 +2,14 @@ package com.globa.homeworknotifier.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.sql.Date
+import java.util.*
 
 @Entity
 data  class Task(
     @PrimaryKey
-    val id : Long,
+    val id : Long?,
     val title : String,
     val description : String,
     val deadline : Date,
-    val subject: Subject
+    val subject: Subject?
 )
