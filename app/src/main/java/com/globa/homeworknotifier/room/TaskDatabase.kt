fe294e18a362146f90a1ee690dc9073a9102249b
Alexander Globa <grobych@gmail.com>
2022-04-29 00:27:53 +0300

    add DateConverter for Room

--- app/src/main/java/com/globa/homeworknotifier/room/TaskDatabase.kt
+++ app/src/main/java/com/globa/homeworknotifier/room/TaskDatabase.kt
@@ -2,9 +2,12 @@ package com.globa.homeworknotifier.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.globa.homeworknotifier.model.Task
+import com.globa.homeworknotifier.util.DateConverter
 
 @Database(entities = [Task::class], version = 1)
+@TypeConverters(DateConverter::class)
 abstract class TaskDatabase : RoomDatabase() {
     abstract val taskDao : TaskDao
 }
