2ae22ee11bdd6213b5a30f9122a15a482e006a80
Alexander Globa <grobych@gmail.com>
2022-05-10 12:44:27 +0300

    switch to var in Task

--- app/src/main/java/com/globa/homeworknotifier/model/Task.kt
+++ app/src/main/java/com/globa/homeworknotifier/model/Task.kt
@@ -8,11 +8,11 @@ import java.util.*
 @Entity
 data  class Task(
     @PrimaryKey
-    val id : Long?,
-    val title : String,
-    val description : String,
-    val deadline : Date,
-    val status: TaskStatus,
+    var id : Long?,
+    var title : String,
+    var description : String,
+    var deadline : Date,
+    var status: TaskStatus,
     @Embedded
-    val subject: Subject?
+    var subject: Subject?
 )
