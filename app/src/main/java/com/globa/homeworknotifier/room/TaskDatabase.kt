70d69108770b827bc30dcd7b08dfeaf1b7ff63d3
Alexander Globa <grobych@gmail.com>
2022-05-10 11:01:44 +0300

    add TaskStatusConverter for Room

--- app/src/main/java/com/globa/homeworknotifier/room/TaskDatabase.kt
+++ app/src/main/java/com/globa/homeworknotifier/room/TaskDatabase.kt
@@ -5,9 +5,10 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.globa.homeworknotifier.model.Task
 import com.globa.homeworknotifier.util.DateConverter
+import com.globa.homeworknotifier.util.TaskStatusConverter
 
-@Database(entities = [Task::class], version = 1)
-@TypeConverters(DateConverter::class)
+@Database(entities = [Task::class], version = 2)
+@TypeConverters(DateConverter::class, TaskStatusConverter::class)
 abstract class TaskDatabase : RoomDatabase() {
     abstract val taskDao : TaskDao
 }
---
+++ app/src/main/java/com/globa/homeworknotifier/util/TaskStatusConverter.kt
@@ -0,0 +1,16 @@
+package com.globa.homeworknotifier.util
+
+import androidx.room.TypeConverter
+import com.globa.homeworknotifier.model.TaskStatus
+
+class TaskStatusConverter {
+    @TypeConverter
+    fun toTaskStatus(string: String): TaskStatus {
+        return TaskStatus.valueOf(string)
+    }
+
+    @TypeConverter
+    fun fromTaskStatus(status: TaskStatus): String {
+        return status.toString()
+    }
+}
